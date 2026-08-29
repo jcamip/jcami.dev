b5d14ba0d3661ef6a2c9ed31032fc6d2c89cc378
Joane Palma <j.palma@webntech.ae>
2026-08-29 22:47:03 +0400

    calhome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect } from "react";
+import { getCalApi } from "@calcom/embed-react";
 import BackToTopButton from "@/components/BackToTopButton";
 import Button from "@/components/Button";
 import PartnersCarousel from "@/components/PartnersCarousel";
@@ -6,7 +10,34 @@ import ScrollToTop from "@/components/ScrollToTop";
 import TypingHeading from "@/components/TypingHeading";
 import styles from "./page.module.css";
 
+const CAL_NAMESPACE = "intro";
+const CAL_LINK = "joane-camille/intro";
+const CAL_CONFIG = JSON.stringify({
+  layout: "month_view",
+  useSlotsViewOnSmallScreen: true,
+  theme: "light",
+});
+
 export default function Home() {
+  useEffect(() => {
+    void (async () => {
+      const cal = await getCalApi({ namespace: CAL_NAMESPACE });
+      cal("ui", {
+        theme: "light",
+        cssVarsPerTheme: {
+          light: {
+            "cal-brand": "#a00000",
+          },
+          dark: {
+            "cal-brand": "#a00000",
+          },
+        },
+        hideEventTypeDetails: true,
+        layout: "month_view",
+      });
+    })();
+  }, []);
+
   return (
     <>
       <ScrollToTop />
@@ -35,11 +66,12 @@ export default function Home() {
           </p>
           <div className={styles.heroCta}>
             <Button
-              href="https://calendly.com/joane-camille/30min"
-              target="_blank"
-              rel="noopener noreferrer"
+              type="button"
               variant="invert"
               className={styles.heroCtaButton}
+              data-cal-namespace={CAL_NAMESPACE}
+              data-cal-link={CAL_LINK}
+              data-cal-config={CAL_CONFIG}
             >
               <span>Start Your Project</span>
               <svg
